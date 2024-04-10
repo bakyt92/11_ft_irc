599eafa321181b2d7513bac520cecb875280daa4
akostrik <internet124816@gmail.com>
2024-04-10 23:16:33 +0200

    *

--- littleServer/serv.cpp
+++ littleServer/serv.cpp
@@ -1,4 +1,17 @@
-# пример из книги Сетевое программирование от Биджа. Использование интернет сокетов.
+// пример из книги Сетевое программирование от Биджа. Использование интернет сокетов.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #define PORT "3490"  // порт для подключения пользователей
 #define BACKLOG 10   // размер очереди ожидающих подключений
