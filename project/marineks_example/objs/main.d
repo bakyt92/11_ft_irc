7ee2a77c99daae1152e04bbdce0aa7937e0a586e
bakyt92 <bakyt92@mail.ru>
2024-04-22 19:28:43 +0300

    fsdfsdfs

---
+++ project/marineks_example/objs/main.d
@@ -0,0 +1,17 @@
+objs/main.o: srcs/main.cpp srcs/../includes/Server.hpp includes/Irc.hpp \
+  includes/Macro.hpp includes/Colors.hpp includes/Numerical_replies.hpp \
+  includes/Client.hpp includes/Channel.hpp
+
+srcs/../includes/Server.hpp:
+
+includes/Irc.hpp:
+
+includes/Macro.hpp:
+
+includes/Colors.hpp:
+
+includes/Numerical_replies.hpp:
+
+includes/Client.hpp:
+
+includes/Channel.hpp:
