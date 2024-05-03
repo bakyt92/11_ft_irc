7290824f4c16bb239e5cd4e4cd8c9832da3582b6
ann <internet124816@gmail.com>
2024-05-03 20:45:09 +0200

    *

--- ann/srcs/utils.cpp
+++ ann/srcs/utils.cpp
@@ -207,11 +207,9 @@ Cli* Server::getCli(string nick) {
 Cli* Server::getCliOnCh(string &nick, string chName) {
   if(getCh(chName) == NULL)
     return NULL;
-  for(set<Cli*>::iterator it = chs[chName]->clis.begin(); it != chs[chName]->clis.end(); it++) {
-    cout << "*** " << (*it)->nick << " on chqnnel?\n";
+  for(set<Cli*>::iterator it = chs[chName]->clis.begin(); it != chs[chName]->clis.end(); it++)
     if(toLower((*it)->nick) == toLower(nick))
       return *it;
-  }
   return NULL;
 }
 
